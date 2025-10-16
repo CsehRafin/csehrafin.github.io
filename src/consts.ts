9d9add7ffd134a14a980679154b1862318054ed8
Ishtiaque Ahmed CSEH Rafin <gamer.mcpe35p0@gmail.com>
2025-10-16 15:02:24 +0600

    CCl

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Root",
-  DESCRIPTION: "Cybersecurity Specialist, ",
+  DESCRIPTION: "Cybersecurity Specialist, PKI Engineer & Architect (ePassports, eID, eMRTDs), and a Researcher",
 };
 
 export const PUBLICATIONS: Metadata = {
